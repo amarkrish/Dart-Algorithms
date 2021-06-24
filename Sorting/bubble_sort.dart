8a7b49b6b5aed82c7c29f6cac9cd7eb36195f2a2
amarkrish <51763282+amarkrish@users.noreply.github.com>
2021-06-24 06:01:42 +0530

    Update bubble_sort.dart

--- Sorting/bubble_sort.dart
+++ Sorting/bubble_sort.dart
@@ -19,25 +19,25 @@
  *
  *  @author : Anantha Natarajan S  
 */
-main() {
-  
-  var a = [3,4,2,6,7,43,2,4,7,9,0,54,4];
-  //var a = [5,4,3,2,1];   
-  for(var i = 0; i < a.length; i++) {
-    bool swaped = false;
-    for(var j = i+1; j<a.length; j++) {
-        if(a[i] > a[j]) {
-          var tmp = a[i];
-          a[i] = a[j];
-          a[j] = tmp;
-          swaped = true;
-        }
+void main() {
+  var a = [3, 4, 2, 6, 7, 43, 2, 4, 7, 9, 0, 54, 4];
+  //var a = [5,4,3,2,1];
+  for (var i = 0; i < a.length; i++) {
+    var swaped = false;
+    for (var j = i + 1; j < a.length; j++) {
+      if (a[i] > a[j]) {
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+        swaped = true;
       }
-     if(!swaped)
-       break;
     }
-  
-  for(var i=0; i<a.length; i++)
+    if (!swaped) {
+      break;
+    }
+  }
+
+  for (var i = 0; i < a.length; i++) {
     print(a[i]);
-  
+  }
 }
